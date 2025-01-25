9a34c207a02a300f8424d2427a8431c9e39b46c5
MarpisGG <marvellc83@gmail.com>
2025-01-26 01:21:22 +0700

    react

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,7 +3,7 @@ import { ProjectCard } from "./ProjectCard";
 import projImg1 from "../assets/img/nusafood.png";
 import projImg2 from "../assets/img/danson.png";
 import projImg3 from "../assets/img/cookit.png";
-import projImg4 from "../assets/img/todolist.png";
+import projImg4 from "../assets/img/todolis.png";
 import colorSharp2 from "../assets/img/color-sharp2.png";
 import 'animate.css';
 import TrackVisibility from 'react-on-screen';
